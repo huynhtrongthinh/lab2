8068af39a037fb45fff193d0ad9f47934633d38e
huynhtrongthinh <162103609+huynhtrongthinh@users.noreply.github.com>
2024-05-19 17:37:22 +0700

    Add files via upload

---
+++ lab 2b3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Tạo danh sách điểm
+        List<float> ldata = new List<float>() { 10, 9, 8, 7.5f, 2, 1.4f };
+
+        // Khởi tạo đối tượng userData
+        userData student = new userData("2024001", "Nguyen Van A");
+
+        // Nhập điểm từ ldata sang student.Scores
+        student.Scores.AddRange(ldata);
+
+        // Hiển thị thông tin của sinh viên
+        Console.WriteLine($"ID: {student.ID}");
+        Console.WriteLine($"UserName: {student.UserName}");
+        Console.WriteLine("Scores: " + string.Join(", ", student.Scores));
+    }
+}
---
+++ lab2 b1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_b1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class UserData
+    {
+        public int id;
+        public int level;
+        public string name;
+        public UserData() 
+        {
+            id = 0;
+            level = 0;
+            name = "null";
+        }
+        public UserData(int id, int level, string name)
+        {
+            this.id = id;
+            this.level = level;
+            this.name = name;
+        }
+        public void XuatThongTin()
+        {
+            Console.WriteLine("Id: " + id);
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("Name: " + name);
+        }
+    }
+    public class Bai1c
+    {
+        public static void XuatThongTin(String attributeName, dynamic attribute)
+        {
+            Console.WriteLine(attributeName + ": " + attribute);
+        }
+    }
+  
+}
